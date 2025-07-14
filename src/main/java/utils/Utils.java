9b1c7405275a2cb7343bd04ef04f0ecc34c50187
Oleg Ch <OChubrik@alfabank.ru>
2025-07-14 10:19:18 +0300

    remove unused methods in Utils

--- src/main/java/utils/Utils.java
+++ src/main/java/utils/Utils.java
@@ -1,32 +1,11 @@
 package utils;
 
 import lombok.experimental.UtilityClass;
-import org.openqa.selenium.WebElement;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Random;
 
 @UtilityClass
 public class Utils {
-
-    public List<String> replaceCharsInList(List<String> listToModify, String[] arrayOfItemsToReplace) {
-        List<String> modifiedList = new ArrayList<>();
-        for (String el : listToModify) {
-            String newValue =
-                    el.replace(arrayOfItemsToReplace[0], " «")
-                            .replace(arrayOfItemsToReplace[1], "»");
-            modifiedList.add(newValue);
-        }
-        return modifiedList;
-    }
-
-    public void replaceWebElementsText(List<WebElement> listWithWebElements, List<String> listWithNewText) {
-        for (int i = 0; i < listWithWebElements.size(); i++) {
-            listWithWebElements.get(i).clear();
-            listWithWebElements.get(i).sendKeys(listWithNewText.get(i));
-        }
-    }
 
     public int generateRandomNumber() {
         Random random = new Random();
