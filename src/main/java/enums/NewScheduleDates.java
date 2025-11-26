28caed7864899e1ac75d0ab8e1cab92578b6e237
Oleg Ch <OChubrik@alfabank.ru>
2025-11-26 11:56:45 +0300

    add new schedule dates

--- src/main/java/enums/NewScheduleDates.java
+++ src/main/java/enums/NewScheduleDates.java
@@ -15,7 +15,9 @@ public enum NewScheduleDates {
     JAN_05("05.01.2025"),
     FEB_14("14.02.2025"),
     MAR_07("07.03.2025"),
-    MAR_08("08.03.2025");
+    MAR_08("08.03.2025"),
+    AUG_31("31.08.2025"),
+    SEPT_01("01.09.2025");
 
     private final String date;
 }
